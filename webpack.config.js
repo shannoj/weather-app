2494d3c5c9c53e9688acd27545a70609b444e0f7
shannoj <69207315+shannoj@users.noreply.github.com>
2023-11-01 14:10:02 +0100

    Revert "getting rid of server elements"
    This reverts commit c8b8f9a49c2db4b68f8be8ce4f03d1546d244f99.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
@@ -13,6 +17,10 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
