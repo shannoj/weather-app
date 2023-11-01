2959fce58e752186f5ae3fedd2d1fdfbc62c79cc
shannoj <69207315+shannoj@users.noreply.github.com>
2023-11-01 14:09:36 +0100

    Revert "trying to deploy"
    This reverts commit 3cebc080895e12cf5249a7555f364e34dc4a738e.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
-  mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
